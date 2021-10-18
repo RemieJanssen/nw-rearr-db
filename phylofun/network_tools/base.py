0084386ec7d1ceaa48e531710c561149a21ae967
RemieJanssen <remiejanssen92@gmail.com>
2021-10-18 16:45:11 +0200

    start network_tools functions

---
+++ phylofun/network_tools/base.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+class Network(nx.DiGraph):
+    def apply_move(move):
+        pass
+
+class RearrangementProblem(object):
+    def __init__(self, network1, network2, move_type)
+        self.network1=network1
+        self.network2=network2
+        self.move_type=move_type
+
+
