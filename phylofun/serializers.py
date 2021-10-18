e8c935c4f99db2bf836a7ffbe782fac7e44c8ab3
RemieJanssen <remiejanssen92@gmail.com>
2021-10-18 18:37:35 +0200

    partially finished network validators

--- phylofun/serializers.py
+++ phylofun/serializers.py
@@ -6,7 +6,7 @@ import networkx as nx
 import ast
 
 class NetworkSerializer(serializers.ModelSerializer):
-    def validate_nodes(self,nodes):
+    def validate_nodes(self, nodes):
         try:
             nodes_list = list(ast.literal_eval(nodes))
             nodes_list_check_ints = [isinstance(x,int) for x in nodes_list]
@@ -15,6 +15,28 @@ class NetworkSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("List of nodes contains non-int values.")
         except serializers.ValidationError as e:
             raise e("List of nodes is not in list format.")
+
+    def validate_edges(self, edges):
+        try: 
+            edge_list = ast.literal_eval(edges)
+            edge_list_check = [
+                isinstance(edge, tuple) 
+                and len(edge)==2 
+                and all([isinstance(x,int) for x in edge])
+                for edge in edge_list
+            ]
+            if all(edge_list_check):
+                return edges
+            raise serializers.ValidationError("List contains an invalid edge.")
+        except ValueError as e:       
+            raise serializers.ValidationError("Not a valid list of edges")
+
+    def save(self):
+        edges = self.validated_data['edges']
+        nodes = set()
+        for e in edges:
+            nodes.update(e)
+          
 
     class Meta:
         model = Network
