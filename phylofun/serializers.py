ef2f7babb9812c0aeaab6ebe0bb3f1a1a8b7e4d1
RemieJanssen <remiejanssen92@gmail.com>
2021-10-18 17:05:08 +0200

    validator for nodes

--- phylofun/serializers.py
+++ phylofun/serializers.py
@@ -2,9 +2,20 @@ from rest_framework import serializers
 from phylofun.models import Network
 from phylofun.models import RearrangementProblem
 from phylofun.models import Solution
-
+import networkx as nx
+import ast
 
 class NetworkSerializer(serializers.ModelSerializer):
+    def validate_nodes(self,nodes):
+        try:
+            nodes_list = list(ast.literal_eval(nodes))
+            nodes_list_check_ints = [isinstance(x,int) for x in nodes_list]
+            if all(nodes_list_check_ints):
+                return nodes_list
+            raise serializers.ValidationError("List of nodes contains non-int values.")
+        except serializers.ValidationError as e:
+            raise e("List of nodes is not in list format.")
+
     class Meta:
         model = Network
         fields = '__all__'
