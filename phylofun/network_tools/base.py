9ea9fd4b5f2e1d048385db1f857b0988b8b678b5
RemieJanssen <remiejanssen92@gmail.com>
2021-10-19 18:00:45 +0200

    Add apply_move method for horizontal moves.

--- phylofun/network_tools/base.py
+++ phylofun/network_tools/base.py
@@ -2,13 +2,44 @@ import networkx as nx
 import ast
 
 class Network(nx.DiGraph):
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges 
+
     def apply_move(self, move):
-        pass
+        """
+        Apply a move to the network.
+        returns True if successful, and False otherwise.        
+        """
+        if self.check_valid(move):
+            if move.type in [MoveType.TAIL, MoveType.HEAD]:
+                if move.moving_node in move.target:
+                    # move does not impact the network
+                    return True
+                self.remove_edges_from([
+                    (move.origin[0], move.moving_node), 
+                    (move.moving_node, move.origin[1]), 
+                    move.target,
+                ])
+                self.add_edges_from([
+                    (move.target[0], move.moving_node), 
+                    (move.moving_node, move.target[1]), 
+                    move.origin,
+                ])
+                return True
+            else:
+                #TODO implement vertical moves
+                return False
+        else:
+            # return False for invalid moves
+            return False
 
     def apply_move_sequence(self, seq_moves):
         for move in seq_moves:
             self.apply_move(move)
 
+    def check_valid(self, move):
+        pass
 
 class RearrangementProblem(object):
     def __init__(self, network1, network2, move_type)
@@ -30,14 +61,19 @@ class MoveType:
 
 class Move(object):
     def __init__(self, move_tuple):
-        self.origin     =move_tuple[0]
-        self.moving_edge=move_tuple[1]
-        self.target     =move_tuple[2]
-        self.moving_node=move_tuple[3]
-        if self.moving_node == self.moving_edge[0]:
-            self.type=MoveType.TAIL
-        else:
-            self.type=MoveType.HEAD
+        try:
+            # horizontal move
+            self.moving_node = move_tuple[3]
+            self.target      = move_tuple[2]
+            self.moving_edge = move_tuple[1]
+            self.origin      = move_tuple[0]            
+            if self.moving_node == self.moving_edge[0]:
+                self.type=MoveType.TAIL
+            else:
+                self.type=MoveType.HEAD
+        except:
+            # vertical move
+            pass
 
 
 class Solution(object):
