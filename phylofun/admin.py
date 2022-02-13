bd1ac6c4e237f1153d1a6c91ab5feae60e04f741
RemieJanssen <remiejanssen92@gmail.com>
2022-02-13 16:00:53 +0100

    Better network serializers

--- phylofun/admin.py
+++ phylofun/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from phylofun.models import (
+    NetworkModel,
+    RearrangementProblemModel,
+    RearrangementSolutionModel,
+)
+
+
+@admin.register(NetworkModel)
+class NetworkAdmin(admin.ModelAdmin):
+    list_display = ("id",)  # TODO add rearrangement problems
+
+
+@admin.register(RearrangementProblemModel)
+class RearrangementProblemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "network1",
+        "network2",
+        "move_type",
+    )  # TODO add best solution length
+
+
+@admin.register(RearrangementSolutionModel)
+class RearrangementSolutionAdmin(admin.ModelAdmin):
+    list_display = ("id", "problem")  # TODO add solution length
--- phylofun/api/network/serializers.py
+++ phylofun/api/network/serializers.py
@@ -1,88 +1,45 @@
-import ast
+import networkx as nx
+from rest_framework import serializers
 
 from phylofun.models import NetworkModel
 from phylofun.network_tools.base import Network
-from rest_framework import serializers
-import networkx as nx
 
 
 class NetworkSerializer(serializers.HyperlinkedModelSerializer):
-    labels = serializers.CharField(default="")
-    nodes = serializers.CharField(default="")
-
-    def validate_nodes(self, nodes):
-        if nodes == "":
-            return []
-        try:
-            nodes_list = list(ast.literal_eval(nodes))
-            nodes_list_check_ints = [isinstance(x, int) for x in nodes_list]
-            if all(nodes_list_check_ints):
-                return nodes_list
-            raise serializers.ValidationError(
-                "List of nodes contains non-int values."
-            )
-        except (SyntaxError, ValueError):
-            raise serializers.ValidationError(
-                "List of nodes is not in list format."
-            )
+    labels = serializers.ListField(
+        child=serializers.ListField(
+            child=serializers.IntegerField(),
+            min_length=2,
+            max_length=2,
+        )
+    )
+    edges = serializers.ListField(
+        child=serializers.ListField(
+            child=serializers.IntegerField(),
+            min_length=2,
+            max_length=2,
+        )
+    )
+    nodes = serializers.ListField(child=serializers.IntegerField())
 
     def validate_edges(self, edges):
-        try:
-            edge_list = ast.literal_eval(edges)
-            edge_list_check = [
-                isinstance(edge, tuple)
-                and len(edge) == 2
-                and all([isinstance(x, int) for x in edge])
-                for edge in edge_list
+        n = Network(edges)
+        if not nx.is_directed_acyclic_graph(n):
+            raise serializers.ValidationError("The network is not acyclic.")
+        if not all(
+            [
+                n.degree(v) < 4 and n.in_degree(v) < 3 and n.out_degree(v) < 3
+                for v in n.nodes
             ]
-            if all(edge_list_check):
-                n = Network(edge_list)
-                if nx.is_directed_acyclic_graph(n):
-                    if all(
-                        [
-                            n.degree(v) < 4
-                            and n.in_degree(v) < 3
-                            and n.out_degree(v) < 3
-                            for v in n.nodes
-                        ]
-                    ):
-                        return edge_list
-                    else:
-                        raise serializers.ValidationError(
-                            "The network is not binary."
-                        )
-                else:
-                    raise serializers.ValidationError(
-                        "The network is not acyclic."
-                    )
-            raise serializers.ValidationError("List contains an invalid edge.")
-        except (SyntaxError, ValueError):
-            raise serializers.ValidationError("Not a valid list of edges")
-
-    def validate_labels(self, labels):
-        if labels == "":
-            return []
-        try:
-            label_list = ast.literal_eval(labels)
-            label_list_check = [
-                isinstance(label, tuple)
-                and len(label) == 2
-                and all([isinstance(x, int) for x in label])
-                for label in label_list
-            ]
-            if all(label_list_check):
-                return labels
-            raise serializers.ValidationError(
-                "List contains an invalid labeling."
-            )
-        except (SyntaxError, ValueError):
-            raise serializers.ValidationError("Not a valid list of labels")
+        ):
+            raise serializers.ValidationError("The network is not binary.")
+        return edges
 
     def save(self):
         n = Network()
         n.add_edges_from(self.validated_data["edges"])
         node_set = set(self.validated_data["nodes"])
-        self.validated_data["nodes"] = list(node_set.union(set(n.nodes)))
+        self.validated_data["nodes"] += list(set(n.nodes).difference(node_set))
         super().save()
 
     class Meta:
