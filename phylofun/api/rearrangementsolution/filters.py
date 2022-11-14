5d8c51e7bcea1374a4556011c9535def682e67fe
RemieJanssen <remiejanssen92@gmail.com>
2022-11-14 22:06:13 +0100

    new filter

--- phylofun/api/rearrangementsolution/filters.py
+++ phylofun/api/rearrangementsolution/filters.py
@@ -1,9 +1,15 @@
+import rest_framework_filters as drf_filters
 from django_filters import rest_framework as filters
 
-from phylofun.models import RearrangementSolutionModel
+from phylofun.models import (RearrangementProblemModel,
+                             RearrangementSolutionModel)
+
+from ..rearrangementproblem.filters import RearrangementProblemFilterSet
 
 
 class RearrangementSolutionFilterSet(filters.FilterSet):
+    problem = drf_filters.RelatedFilter(RearrangementProblemFilterSet, queryset=RearrangementProblemModel.objects.all(), lookups="__all__")
+
     class Meta:
         model = RearrangementSolutionModel
         fields = {
